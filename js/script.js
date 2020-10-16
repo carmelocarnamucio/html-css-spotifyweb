76287618ac543150ac8622d61f8c6d76aeb68c42
carmelocarnamucio <carnamuciocarmelo@gmail.com>
2020-10-16 14:46:49 +0200

    aggiunto javascript e fix

---
+++ js/script.js
@@ -0,0 +1,5 @@
+// Nome profilo
+document.getElementById('my-user').innerHTML = 'Carmelo Carnamucio';
+
+// Nome playlist personalizzate
+document.getElementById('my-user-playlist').innerHTML = 'Creato per Carmelo Carnamucio';
